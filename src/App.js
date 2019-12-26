10758252e236fa825c23c70be9376cf44ba7e921
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2019-12-26 16:49:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
 function App() {
   return (
@@ -23,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
